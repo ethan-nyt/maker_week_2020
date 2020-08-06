6d4eccc5de64eba7407d026f73503aaa024db07f
Enmanuel de la Nuez <edelanuez@pm.me>
2020-08-06 11:55:04 -0400

    Setup (#1)
    * installed prettier, class to fn component
    
    * add semantic

--- src/App.js
+++ src/App.js
@@ -1,62 +1,78 @@
-import React from 'react';
-import Chart from 'chart.js'
+import React, { useRef, useEffect, useState } from "react";
+import Chart from "chart.js";
+import { Button } from "semantic-ui-react";
 
-export default class App extends React.Component {
-  constructor() {
-    super()
-    this.canvasRef = React.createRef();
-  }
-  componentDidMount() {
-    this.myChart = new Chart(this.canvasRef.current, {
-      type: 'pie',
+import axios from "axios";
+
+export default () => {
+  const canvasRef = useRef(null);
+  const [apiMsg, setApiMsg] = useState("");
+  let myChart;
+
+  console.log(apiMsg);
+
+  const getAPIMsg = () => {
+    axios.get("http://localhost:5000").then(setApiMsg);
+  };
+
+  useEffect(() => {
+    myChart = new Chart(canvasRef.current, {
+      type: "pie",
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1,
-          id: [1, 2, 3, 4, 5, 6],
-        }]
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+            ],
+            borderWidth: 1,
+            id: [1, 2, 3, 4, 5, 6],
+          },
+        ],
       },
       options: {
-        onClick: this.onChartClick,
+        onClick: onChartClick,
         // events: ['click'],
         scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      },
     });
-  }
-  onChartClick = (event, item) => {
-    this.myChart.update()
-    item[0]._model.outerRadius += 10
-    console.log(item[0]._model)
-  }
-  render() {
-    return (
-      <div id="app">
-        <canvas ref={this.canvasRef} id="canvas"></canvas>
-      </div>
-    );
-  }
-}
+
+    getAPIMsg();
+  }, []);
+
+  const onChartClick = (event, item) => {
+    myChart.update();
+    item[0]._model.outerRadius += 10;
+    console.log(item[0]._model);
+  };
+
+  return (
+    <div id="app">
+      <Button />
+      <canvas ref={canvasRef} id="canvas"></canvas>
+    </div>
+  );
+};
