4b8e1e72447d4007e0f68aaf816da000dfc426ee
Enmanuel de la Nuez <edelanuez@pm.me>
2020-08-07 11:49:33 -0400

    add conditions to bubbles

--- src/components/Bubbles.js
+++ src/components/Bubbles.js
@@ -1,35 +1,39 @@
-import React            from 'react';
-import ReactBubbleChart from 'react-bubble-chart';
-import 'react-bubble-chart/src/style.css';
-import '../App.css';
+import React from "react";
+import ReactBubbleChart from "react-bubble-chart";
+import "react-bubble-chart/src/style.css";
+import "../App.css";
 
-var colorLegend = [
-  //reds from dark to light
-  {color: "#67000d", text: 'Negative', textColor: "#ffffff"}, "#a50f15", "#cb181d", "#ef3b2c", "#fb6a4a", "#fc9272", "#fcbba1", "#fee0d2",
-  //neutral grey
-  {color: "#f0f0f0", text: 'Neutral'},
-  // blues from light to dark
-  "#deebf7", "#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#08519c", {color: "#08306b", text: 'Positive', textColor: "#ffffff"}
+// var colorLegend = [
+//   //reds from dark to light
+//   {color: "#67000d", text: 'Negative', textColor: "#ffffff"}, "#a50f15", "#cb181d", "#ef3b2c", "#fb6a4a", "#fc9272", "#fcbba1", "#fee0d2",
+//   //neutral grey
+//   {color: "#f0f0f0", text: 'Neutral'},
+//   // blues from light to dark
+//   "#deebf7", "#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#08519c", {color: "#08306b", text: 'Positive', textColor: "#ffffff"}
+// ];
+
+var tooltipProps = [
+  {
+    css: "symbol",
+    prop: "_id",
+  },
+  {
+    css: "value",
+    prop: "value",
+    display: "Last Value",
+  },
+  {
+    css: "change",
+    prop: "colorValue",
+    display: "Change",
+  },
 ];
-
-var tooltipProps = [{
-  css: 'symbol',
-  prop: '_id'
-}, {
-  css: 'value',
-  prop: 'value',
-  display: 'Last Value'
-}, {
-  css: 'change',
-  prop: 'colorValue',
-  display: 'Change'
-}];
 
 const mockData = [
   {
     value: 2000,
     sentiment: 2,
-    selected: false
+    selected: false,
   },
   {
     value: 15,
@@ -44,7 +48,7 @@ const mockData = [
   {
     value: 4,
     sentiment: 4,
-    selected: true,
+    selected: false,
   },
   {
     value: 5,
@@ -54,25 +58,42 @@ const mockData = [
   {
     value: 3,
     sentiment: 3,
-    selected: false
+    selected: false,
   },
   {
     value: 1,
     sentiment: 7,
     selected: false,
-  }
-]
+  },
+];
+// TODO: follow same order as floats from api
+var conditions = [
+  "Ruptured Uterus",
+  "Perineal Laceration",
+  "Transfusion",
+  "Intensive Care",
+  "Unplanned Historectemy",
+];
 
 export default class BubbleChart extends React.Component {
-  render () {
-    var data = mockData.map((d, i) => ({
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    var newData = this.props.results.map((float) => ({
+      value: float,
+      sentiment: Math.random(),
+      selected: false,
+    }));
+    var data = newData.map((d, i) => ({
       _id: i,
       value: d.value,
+      displayText: conditions[i],
       colorValue: d.sentiment,
       selected: d.selected,
-      foo: 'bar'
+      foo: "bar",
     }));
-    
+
     return (
       <div>
         <ReactBubbleChart
@@ -81,15 +102,16 @@ export default class BubbleChart extends React.Component {
           data={data}
           selectedColor="#737373"
           selectedTextColor="#d9d9d9"
-          fixedDomain={{min: -1, max: 1}}
-          onClick={(e) => console.log('click', e)}
+          fixedDomain={{ min: -1, max: 1 }}
+          onClick={(e) => console.log("click", e)}
           legend={true}
           legendSpacing={0}
+          // fontSizeFactor={1.25}
           tooltip={true}
           tooltipProps={tooltipProps}
           // tooltipFunc={tooltipFunc}
         />
       </div>
-    )
+    );
   }
 }
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,34 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Button, Icon, Loader } from "semantic-ui-react";
-import axios from 'axios';
-import BubbleChart from './Bubbles'
-
+import axios from "axios";
+import BubbleChart from "./Bubbles";
 
 const mockResponse = {
-  data: [0.05, 0.15, 0.25, 0.5, 0.05]
+  data: [0.05, 0.15, 0.25, 0.5, 0.05],
 };
 
-const wait = (ms) => new Promise(resolve => setTimeout(() => resolve(mockResponse), ms));
+const wait = (ms) =>
+  new Promise((resolve) => setTimeout(() => resolve(mockResponse), ms));
 
 export default ({ userDetails }) => {
   const { age, race, hospitalVisits } = userDetails;
-  const [response, setResponse] = useState(null)
+  const [response, setResponse] = useState(null);
   // if details are true, send to API!
   useEffect(() => {
-    const payload = { input: [race, hospitalVisits, age] }
+    const payload = { input: [race, hospitalVisits, age] };
     // TODO replace wait with the actual axios.post when the server is ready
     // axios.post('/api/predict', payload).then(console.log).catch(console.error)
-    wait(3000).then(({ data }) => {
+    wait(1500).then(({ data }) => {
       //
-      setResponse(data)
-    })
-  }, [])
+      setResponse(data);
+    });
+  }, []);
   return (
     <div>
-      {
-        response ? <BubbleChart /> : <Loader size="massive" active={true} />
-      }
+      {response ? (
+        <BubbleChart results={response} />
+      ) : (
+        <Loader size="massive" active={true} />
+      )}
       <p>
         Age: {age}, Race: {race}, Prenatal visits: {hospitalVisits}
       </p>
