346605cbf71da2b4c43a14c93ad2fb27fcfa6328
Lipkind <ethan.lipkind@nytimes.com>
2020-08-07 11:24:58 -0400

    make the bubbles pink

--- src/components/Bubbles.js
+++ src/components/Bubbles.js
@@ -3,14 +3,14 @@ import ReactBubbleChart from 'react-bubble-chart';
 import 'react-bubble-chart/src/style.css';
 import '../App.css';
 
-// var colorLegend = [
-//   //reds from dark to light
-//   {color: "#67000d", text: 'Negative', textColor: "#ffffff"}, "#a50f15", "#cb181d", "#ef3b2c", "#fb6a4a", "#fc9272", "#fcbba1", "#fee0d2",
-//   //neutral grey
-//   {color: "#f0f0f0", text: 'Neutral'},
-//   // blues from light to dark
-//   "#deebf7", "#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#08519c", {color: "#08306b", text: 'Positive', textColor: "#ffffff"}
-// ];
+var colorLegend = [
+  //reds from dark to light
+  {color: "#67000d", text: 'Negative', textColor: "#ffffff"}, "#a50f15", "#cb181d", "#ef3b2c", "#fb6a4a", "#fc9272", "#fcbba1", "#fee0d2",
+  //neutral grey
+  {color: "#f0f0f0", text: 'Neutral'},
+  // blues from light to dark
+  "#deebf7", "#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#08519c", {color: "#08306b", text: 'Positive', textColor: "#ffffff"}
+];
 
 var tooltipProps = [{
   css: 'symbol',
