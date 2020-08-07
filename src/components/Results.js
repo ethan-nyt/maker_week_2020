d3091d0bb0894328df5122299fa45a9e4e60ecd0
Lipkind <ethan.lipkind@nytimes.com>
2020-08-07 10:49:54 -0400

    conditionally render a mock response

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, Icon } from "semantic-ui-react";
+import { Button, Icon, Loader } from "semantic-ui-react";
+import axios from 'axios';
+
+
+const mockResponse = {
+  data: [0.05, 0.15, 0.25, 0.5, 0.05]
+};
+
+const wait = (ms) => new Promise(resolve => setTimeout(() => resolve(mockResponse), ms));
 
 export default ({ userDetails }) => {
   const { age, race, hospitalVisits } = userDetails;
+  const [response, setResponse] = useState(null)
   // if details are true, send to API!
+  useEffect(() => {
+    const payload = { input: [race, hospitalVisits, age] }
+    // axios.post('/api/predict', payload).then(console.log).catch(console.error)
+    wait(3000).then(({ data }) => {
+      //
+      setResponse(data)
+    })
+  }, [])
   return (
     <div>
-      <p>Results coming up!</p>
+      {
+        response ? JSON.stringify(response) : <Loader size="massive" active={true} />
+      }
       <p>
         Age: {age}, Race: {race}, Prenatal visits: {hospitalVisits}
       </p>
